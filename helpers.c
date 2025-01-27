19dba3ba50e3c931d5dc25da0e4f6065e70eb65a
KMPG82 <kevinponting82@gmail.com>
2025-01-26 22:03:33 -0800

    I'm tired

--- helpers.c
+++ helpers.c
@@ -4,7 +4,8 @@
 /* prints the distribution of cuts along with how much value they contributed to
 the total, the remainder, and the most value that can be obtained */
 void print_results(const int LENGTH_OPTIONS[], const int CUTS[],
-                   const int VALUES[], const int NUMBER_OF_OPTIONS, const int REMAINDER, const int BEST_VALUE) {
+                   const int VALUES[], const int NUMBER_OF_OPTIONS,
+                   const int REMAINDER, const int BEST_VALUE) {
     printf("\nCutting list (<n> @ <size> = <total value>): \n");
     for (int ix = 0; ix < NUMBER_OF_OPTIONS; ix++) {
         const int TOTAL_VALUE = (CUTS[ix] * VALUES[ix]);
@@ -20,18 +21,21 @@ void input_cut_options(int **length_options, int **values,
                        int *number_of_options) {
     int length;
     int value;
+    char buffer[100];
 
     printf(
         "Enter the list of piece prices in the format <length>, "
         "<value> (Ctrl+Z for Windows / Ctrl+D for Linux to end):\n");
 
-    while (scanf("%d %d", &length, &value) != EOF) {
-        *length_options =
-            realloc(*length_options, (*number_of_options + 1) * sizeof(int));
-        *values = realloc(*values, (*number_of_options + 1) * sizeof(int));
+    while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        if (sscanf(buffer, "%d, %d", &length, &value) == 2) {
+            *length_options = realloc(*length_options,
+                                      (*number_of_options + 1) * sizeof(int));
+            *values = realloc(*values, (*number_of_options + 1) * sizeof(int));
 
-        (*length_options)[*number_of_options] = length;
-        (*values)[*number_of_options]         = value;
-        (*number_of_options)++;
+            (*length_options)[*number_of_options] = length;
+            (*values)[*number_of_options]         = value;
+            (*number_of_options)++;
+        }
     }
 }
